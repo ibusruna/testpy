e1fab4fc2346cb51394580aa64bb39be63d33d9b
ibusruna <ibusruna@gmail.com>
2023-06-07 21:21:31 +0300

    variables

--- a.py
+++ a.py
@@ -1,4 +1,18 @@
-print(5//3, "\"", end="\n")
-print(min(1, 2, 4, 5), max(2, 10))
-print(pow(5, 3))
-print(round(5 / 3))
+# print(5//3, "\"", end="\n")
+# print(min(1, 2, 4, 5), max(2, 10))
+# print(pow(5, 3))
+# print(round(5 / 3))
+
+####### Lesson #4 
+x = 5
+
+del x
+
+y = 1.3
+print(y)
+
+b = True
+b = False
+print(b)
+
+####### 
