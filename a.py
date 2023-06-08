92c922205bc8eb964109a92f2facbf7c9389129b
ibusruna <ibusruna@gmail.com>
2023-06-08 19:15:12 +0300

    conditional statements

--- a.py
+++ a.py
@@ -4,15 +4,29 @@
 # print(round(5 / 3))
 
 ####### Lesson #4 
-x = 5
+# x = 5
 
-del x
+# del x
 
-y = 1.3
-print(y)
+# y = 1.3
+# print(y)
 
-b = True
-b = False
-print(b)
+# b = True
+# b = False
+# print(b)
 
-####### 
+####### Conditional Statements (if elif else)
+x = 1
+y = 2
+
+if x == 1 and y == 2 or x == 1 and y == 3:
+    print("yes")
+
+elif x == 1:
+    print("yes")
+
+else:
+    print("no")
+
+a = 5 if x ==1 else 0 #this is a ternary operator
+print(a)
